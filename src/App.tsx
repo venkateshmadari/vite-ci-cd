a652333469b4bb3a74c6376c3b12f09527fdb395
YAKUB MOODU <140063064+yakub2016@users.noreply.github.com>
2026-01-02 17:47:32 +0530

    Add greeting for Venkatesh

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
     <>
       <div>
         <h1>Hello</h1>
+        <h1>Venkatesh</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
